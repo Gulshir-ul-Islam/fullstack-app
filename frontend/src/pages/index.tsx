c1bc1e3744d8572558f3100d8443b60119314812
Gulshir ul Islam <gulshirislam@gmail.com>
2024-01-07 15:21:21 +0530

    Dockerize frontend app

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import CardComponent from '../components/CardComponent';
+import CardComponent from '../components/cardComponent';
 
 interface User {
   id: number;
